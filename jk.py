38b07975362208311fba3f94f45a9022d667f066
Nowak-H <marta.judyta.nowak@gmail.com>
2025-09-27 15:55:26 +0200

    Update jk.py

--- jk.py
+++ jk.py
@@ -1,90 +1,112 @@
-   import math
+import math
 
-Lista do przechowwywania historiiobliczeń
-historia =[]
-def menu_plaskie():
-print("""
-=== FIGURY PŁASKIE ===
-a - Koło
-b - Kwadrat
-c - Prostokąt
-d - Trójkąt (Heron)
-e - Trójkąt równoboczny
-f - Trapez
-g - Równoległobok
-h - Romb
-    elif fig == "b":
-        a = float(input("Bok a = "))
-        print(f"Pole kwadratu = {round(a**2, 2)}")
-        print(f"Obwód kwadratu = {round(4 * a, 2)}")
-        print(f"Przekątna kwadratu = {round(a * math.sqrt(2), 2)}")
-
-    elif fig == "c":
-        a = float(input("Bok a = "))
-        b = float(input("Bok b = "))
-        print(f"Pole prostokąta = {round(a * b, 2)}")
-        print(f"Obwód prostokąta = {round(2 * (a + b), 2)}")
-
-    elif fig == "d":
-        a = float(input("Bok a = "))
-        b = float(input("Bok b = "))
-        c = float(input("Bok c = "))
-        obw = a + b + c
-        if a + b > c and a + c > b and b + c > a:
-            s = obw / 2
-            pole = math.sqrt(s * (s - a) * (s - b) * (s - c))
-            print(f"Obwód trójkąta = {round(obw, 2)}")
-            print(f"Pole trójkąta = {round(pole, 2)}")
-        else:
-            print("Z tych boków nie da się zbudować trójkąta.")
-
-    elif fig == "e":
-        a = float(input("Bok a = "))
-        pole = (a**2 * math.sqrt(3)) / 4
-        obw = 3 * a
-        h = (a * math.sqrt(3)) / 2
-        print(f"Pole trójkąta równobocznego = {round(pole, 2)}")
-        print(f"Obwód trójkąta równobocznego = {round(obw, 2)}")
-        print(f"Wysokość trójkąta równobocznego = {round(h, 2)}")
-
-    elif fig == "f":
-        a = float(input("Podstawa a = "))
-        b = float(input("Podstawa b = "))
-        h = float(input("Wysokość h = "))
-        pole = ((a + b) * h) / 2
-        c = float(input("Bok c = "))
-        d = float(input("Bok d = "))
-        obw = a + b + c + d
-        print(f"Pole trapezu = {round(pole, 2)}")
-        print(f"Obwód trapezu = {round(obw, 2)}")
-
-    elif fig == "g":
-        a = float(input("Bok a = "))
-        b = float(input("Bok b = "))
-        h = float(input("Wysokość h do boku a = "))
-        print(f"Pole równoległoboku = {round(a * h, 2)}")
-        print(f"Obwód równoległoboku = {round(2 * (a + b), 2)}")
-
-    elif fig == "h":
-        print("Jak chcesz policzyć pole rombu?")
-        print("1 - P = a * h (bok i wysokość)")
-        print("2 - P = (e * f) / 2 (przekątne)")
-        wybor = input("1 / 2 ? ")
-        if wybor == "1":
-            a = float(input("Bok a = "))
-            h = float(input("Wysokość h = "))
-            print(f"Pole rombu = {round(a * h, 2)}")
-            print(f"Obwód rombu = {round(4 * a, 2)}")
-        elif wybor == "2":
-            a = float(input("Bok a = "))
-            e = float(input("Przekątna e = "))
-            f = float(input("Przekątna f = "))
-            print(f"Pole rombu = {round((e * f) / 2, 2)}")
-            print(f"Obwód rombu = {round(4 * a, 2)}")
-        else:
-            print("Nie ma takiej opcji.")
-    else:
-        print("Nie ma takiej figury.")
+def get_float(prompt):
+while True:
+try:
+value = float(input(prompt))
+if value > 0:
+return value
+print("Podaj liczbę większą od 0.")
 except ValueError:
-    print("Błąd: podaj prawidłową liczbę.")
-input("Naciśnij Enter, aby wrócić do menu...")  # PAUZA
+print("To nie jest liczba!")
+
+def figury_plaskie():
+print("a - Koło\nb - Kwadrat\nc - Prostokąt\nd - Trójkąt\ne - Trapez\nf - Równoległobok\ng - Romb")
+wybor = input("Wybierz figurę: ").lower()
+if wybor == "a":
+r = get_float("Promień: ")
+print("Pole koła:", round(math.pi * r2, 2))
+print("Obwód koła:", round(2 * math.pi * r, 2))
+elif wybor == "b":
+a = get_float("Bok: ")
+print("Pole kwadratu:", round(a2, 2))
+print("Obwód kwadratu:", round(4 * a, 2))
+elif wybor == "c":
+a = get_float("Bok a: ")
+b = get_float("Bok b: ")
+print("Pole prostokąta:", round(a * b, 2))
+print("Obwód prostokąta:", round(2 * (a + b), 2))
+elif wybor == "d":
+a = get_float("Bok a: ")
+h = get_float("Wysokość: ")
+b = get_float("Bok b: ")
+c = get_float("Bok c: ")
+print("Pole trójkąta:", round((a * h) / 2, 2))
+print("Obwód trójkąta:", round(a + b + c, 2))
+elif wybor == "e":
+a = get_float("Podstawa a: ")
+b = get_float("Podstawa b: ")
+h = get_float("Wysokość: ")
+c = get_float("Bok c: ")
+d = get_float("Bok d: ")
+print("Pole trapezu:", round(((a + b) * h) / 2, 2))
+print("Obwód trapezu:", round(a + b + c + d, 2))
+elif wybor == "f":
+a = get_float("Bok a: ")
+b = get_float("Bok b: ")
+h = get_float("Wysokość do a: ")
+print("Pole równoległoboku:", round(a * h, 2))
+print("Obwód równoległoboku:", round(2 * (a + b), 2))
+elif wybor == "g":
+a = get_float("Bok: ")
+h = get_float("Wysokość: ")
+d1 = get_float("Przekątna 1: ")
+d2 = get_float("Przekątna 2: ")
+print("Pole rombu:", round((d1 * d2) / 2, 2))
+print("Obwód rombu:", round(4 * a, 2))
+
+def bryly():
+print("a - Sześcian\nb - Prostopadłościan\nc - Walec\nd - Kula\ne - Stożek\nf - Graniastosłup\ng - Ostrosłup")
+wybor = input("Wybierz bryłę: ").lower()
+if wybor == "a":
+a = get_float("Bok: ")
+print("Pole powierzchni sześcianu:", round(6 * a2, 2))
+print("Objętość sześcianu:", round(a3, 2))
+elif wybor == "b":
+a = get_float("Bok a: ")
+b = get_float("Bok b: ")
+c = get_float("Bok c: ")
+print("Pole powierzchni prostopadłościanu:", round(2 * (ab + ac + b*c), 2))
+print("Objętość prostopadłościanu:", round(a * b * c, 2))
+elif wybor == "c":
+r = get_float("Promień podstawy: ")
+h = get_float("Wysokość: ")
+print("Pole powierzchni walca:", round(2 * math.pi * r * (r + h), 2))
+print("Objętość walca:", round(math.pi * r2 * h, 2))
+elif wybor == "d":
+r = get_float("Promień: ")
+print("Pole powierzchni kuli:", round(4 * math.pi * r2, 2))
+print("Objętość kuli:", round((4/3) * math.pi * r3, 2))
+elif wybor == "e":
+r = get_float("Promień podstawy: ")
+h = get_float("Wysokość: ")
+l = math.sqrt(r2 + h2)
+print("Pole powierzchni stożka:", round(math.pi * r * (r + l), 2))
+print("Objętość stożka:", round((1/3) * math.pi * r2 * h, 2))
+elif wybor == "f":
+pp = get_float("Pole podstawy: ")
+obw = get_float("Obwód podstawy: ")
+h = get_float("Wysokość: ")
+print("Pole powierzchni graniastosłupa:", round(2 * pp + obw * h, 2))
+print("Objętość graniastosłupa:", round(pp * h, 2))
+elif wybor == "g":
+pp = get_float("Pole podstawy: ")
+obw = get_float("Obwód podstawy: ")
+l = get_float("Tworząca: ")
+h = get_float("Wysokość: ")
+print("Pole powierzchni ostrosłupa:", round(pp + (obw * l) / 2, 2))
+print("Objętość ostrosłupa:", round((1/3) * pp * h, 2))
+
+def main():
+print("\nKALKULATOR GEOMETRII")
+print("a - Płaskie\nb - Bryły")
+wybor = input("Wybierz a - płaskie, b - bryły: ").lower()
+if wybor == "a":
+figury_plaskie()
+elif wybor == "b":
+bryly()
+else:
+print("Nieprawidłowa opcja.")
+
+if name == "main":
+main()
